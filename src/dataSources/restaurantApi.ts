6049a7c81dd0690c40bbc0eb4844a26cb91250e4
kei <tenguuenglish@gmail.com>
2020-02-16 06:04:06 +0900

    :recycle: Fix datasources

--- src/dataSources/restaurantApi.ts
+++ src/dataSources/restaurantApi.ts
@@ -1,5 +1,5 @@
 import { RESTDataSource, RequestOptions } from 'apollo-datasource-rest';
-import { FilterInput } from '../types/graphql';
+import { FilterInput, GetRestaurantsResponse } from '../types/graphql';
 import { ResponseGetRestaurantsApi } from './models/restaurantApi';
 
 class RestaurantAPI extends RESTDataSource {
@@ -9,22 +9,39 @@ class RestaurantAPI extends RESTDataSource {
   }
 
   public willSendRequest(request: RequestOptions) {
-    request.params.set('keyid', process.env.GNAVI_API_ACCESS_KEYID as string);
+    request.params.set('keyid', process.env.GNAVI_API_ACCESS_KEYID || '');
   }
 
-  public async getRestaurants(input: FilterInput): Promise<ResponseGetRestaurantsApi> {
-    return this.get('/', {
+  public async getRestaurants(input: FilterInput) {
+    const response = await this.get<ResponseGetRestaurantsApi>('/', {
+      freeword: input.freeword ?? '',
       ...(input.latitude ? { latitude: input.latitude } : {}),
       ...(input.longitude ? { longitude: input.longitude } : {}),
       ...(input.range ? { range: input.range } : {}),
       ...(input.offsetPage ? { offset_page: input.offsetPage } : {}),
-      freeword: input.freeword ?? '',
       ...(input.lunch ? { lunch: input.lunch } : {}),
       ...(input.bottomLessCup ? { bottomless_cup: input.bottomLessCup } : {}),
       ...(input.buffet ? { buffet: input.buffet } : {}),
       ...(input.privateRoom ? { private_room: input.privateRoom } : {}),
       ...(input.webReserve ? { web_reserve: input.webReserve } : {}),
     });
+
+    return {
+      restaurants: response.rest.map(({ id, name, image_url, opentime, access, lunch, budget }) => ({
+        id,
+        name,
+        lunch: lunch ? lunch : null,
+        budget: budget ? budget : null,
+        image: image_url.shop_image1,
+        openTime: opentime,
+        nearStation: access.station,
+      })),
+      pageInfo: {
+        totalCount: response.total_hit_count,
+        perPage: response.hit_per_page,
+        currentPage: response.page_offset,
+      },
+    };
   }
 }
 
--- src/schema/resolver.ts
+++ src/schema/resolver.ts
@@ -2,26 +2,8 @@ import { Resolvers } from '../types/graphql';
 
 const resolvers: Resolvers = {
   Query: {
-    restaurants: async (_, { input }, { dataSources }) => {
-      const response = await dataSources.restaurantApi.getRestaurants(input ?? { freeword: '' });
-
-      return {
-        restaurants: response.rest.map(({ id, name, image_url, opentime, access, lunch, budget }) => ({
-          id,
-          name,
-          lunch: lunch ? lunch : null,
-          budget: budget ? budget : null,
-          image: image_url.shop_image1,
-          openTime: opentime,
-          nearStation: access.station,
-        })),
-        pageInfo: {
-          totalCount: response.total_hit_count,
-          perPage: response.hit_per_page,
-          currentPage: response.page_offset,
-        },
-      };
-    },
+    restaurants: async (_, { input }, { dataSources }) =>
+      await dataSources.restaurantApi.getRestaurants(input ?? { freeword: '' }),
   },
 };
 
